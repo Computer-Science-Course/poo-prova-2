4c68bed20eef5b632443c18f9ffe65acc1ffa66d
Itala Vitoria <italavicky35@gmail.com>
2022-09-05 09:24:43 -0400

    Adicao da Classe Locacao

---
+++ src/model/entities/locacao/Locacao.java
@@ -0,0 +1,46 @@
+package model.entities.locacao;
+
+import java.time.LocalDateTime;
+
+public class Locacao {
+	private Integer id;
+	private LocalDateTime dataRetirada;
+	private LocalDateTime dataDevolucao;
+	
+	public Locacao() {
+		
+	}
+
+	public Locacao(Integer id, LocalDateTime dataRetirada, LocalDateTime dataDevolucao) {
+		this.id = id;
+		this.dataRetirada = dataRetirada;
+		this.dataDevolucao = dataDevolucao;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public LocalDateTime getDataRetirada() {
+		return dataRetirada;
+	}
+
+	public void setDataRetirada(LocalDateTime dataRetirada) {
+		this.dataRetirada = dataRetirada;
+	}
+
+	public LocalDateTime getDataDevolucao() {
+		return dataDevolucao;
+	}
+
+	public void setDataDevolucao(LocalDateTime dataDevolucao) {
+		this.dataDevolucao = dataDevolucao;
+	}
+	
+	
+	
+}
