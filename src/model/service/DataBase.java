54587f22f7cb0c9ae217c37778e770ae49e3d8e4
Mateus Santos <a.mateussm1.5@gmail.com>
2022-09-06 12:20:10 -0300

    :wrench: | Database model update

--- src/model/service/DataBase.java
+++ src/model/service/DataBase.java
@@ -4,34 +4,35 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 public class DataBase {
 	
-	private static Connection conn = null;
+private static Connection conn = null;
 	
 	public static Connection getConnection() {
 		if (conn == null) {
 			try {
 				Properties props = loadProperties();
 				String url = props.getProperty("dburl");
-				conn = DriverManager.getConnection(url, props);				
+				conn = DriverManager.getConnection(url, props);
 			}
 			catch (SQLException e) {
-				throw new DataBaseException(e.getMessage());
+				throw new DbException(e.getMessage());
 			}
 		}
 		return conn;
 	}
 	
-	public static void closeConnectrion() {
+	public static void closeConnection() {
 		if (conn != null) {
 			try {
-				conn.close();				
-			}
-			catch (SQLException e) {
-				throw new DataBaseException(e.getMessage());
+				conn.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
 			}
 		}
 	}
@@ -43,7 +44,27 @@ public class DataBase {
 			return props;
 		}
 		catch (IOException e) {
-			throw new DataBaseException(e.getMessage());
-		}	
+			throw new DbException(e.getMessage());
+		}
+	}
+	
+	public static void closeStatement(Statement st) {
+		if (st != null) {
+			try {
+				st.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+
+	public static void closeResultSet(ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
 	}
 }
