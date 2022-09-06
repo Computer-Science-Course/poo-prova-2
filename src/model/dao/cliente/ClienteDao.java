3fe988e2cd5ea9953725a044e739bf9d663a29f1
Mateus Santos <a.mateussm1.5@gmail.com>
2022-09-05 22:09:55 -0300

    :sparkles: | ClienteDao: Giving argumente Cliente to method criarCliente

--- src/model/dao/cliente/ClienteDao.java
+++ src/model/dao/cliente/ClienteDao.java
@@ -6,7 +6,7 @@ import model.entities.cliente.Cliente;
 
 
 public interface ClienteDao {
-	void criarCliente();
+	void criarCliente(Cliente cliente);
 	List<Cliente> listarTodosClientes();
 	void editarCliente(Integer id);
 	void excluirCliente(Integer id);
